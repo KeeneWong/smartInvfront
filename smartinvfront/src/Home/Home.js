0859234aa8c49f518d1338d855c785fa6484baee
keenewong <keenewonghofung@gmail.com>
2019-09-23 13:04:11 -0400

    responsive

--- smartinvfront/src/Home/Home.js
+++ smartinvfront/src/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ItemContainer from '../ItemContainer/ItemContainer';
 import Welcome from '../Welcome/Welcome';
+import './Home.css';
 
 class Home extends Component {
     render() {
@@ -8,7 +9,7 @@ class Home extends Component {
             this.props.history.push("/login");
         }
         return (
-            <div className="homeMain flexrow test">
+            <div className="homeMain flexrow">
                 <Welcome items={this.props.items} username={this.props.username}></Welcome>
                 <ItemContainer token={this.props.token}></ItemContainer>
 
