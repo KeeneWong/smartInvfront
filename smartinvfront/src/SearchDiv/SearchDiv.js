f44fdbda5a637cf6127eb5ebc4d2ef4c2cdde314
keenewong <keenewonghofung@gmail.com>
2019-10-01 17:00:10 -0400

    phone size

--- smartinvfront/src/SearchDiv/SearchDiv.js
+++ smartinvfront/src/SearchDiv/SearchDiv.js
@@ -8,13 +8,6 @@ class SearchDiv extends Component {
             <div className="searchDivMain flexcolumn">
                 <h3 className="itemsearchh3">Item Search</h3>
 
-
-                <DropdownButton id="dropdown-basic-button" title="Catergory" key="Secondary">
-                    <Dropdown.Item href="#">Action</Dropdown.Item>
-                    <Dropdown.Item href="#">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#">Something else</Dropdown.Item>
-                </DropdownButton>
-
                 <InputGroup size="sm" className="mb-3 searchfield">
                     <InputGroup.Prepend>
                         <InputGroup.Text id="inputGroup-sizing-sm">Search</InputGroup.Text>
